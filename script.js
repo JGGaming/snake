3f76dd08961578139abe74425687d095bb238db9
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-10-07 16:37:05 -0400

    add variables for rows, columns, margin, and square

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const ctx = cvs.getContext("2d");
 
 const height = cvs.height;
 const width = cvs.width;
+
+const row = 20;
+const col = 20;
+const margin = 1;
+const sq = height / (row + margin * 2);

89ad0882ca5b489742e366ea986e228b28eb3ab0
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-10-07 16:39:55 -0400

    add color variables

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const row = 20;
 const col = 20;
 const margin = 1;
 const sq = height / (row + margin * 2);
+
+const COLOR_LIGHTGREEN = "#90EE90";
+const COLOR_GREEN = "#0F0";
+const COLOR_DARKGREEN = "#006400";
+const COLOR_BLACK = "#000000";
+const COLOR_WHITE = "#FFFFFF";
+const COLOR_RED = "#F00";

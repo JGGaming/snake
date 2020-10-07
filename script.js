8e1b872eb2f2ecdafa51edcd68d536cb89ebe517
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-10-07 17:54:26 -0400

    add drawBoard function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const width = cvs.width;
 
 const row = 20;
 const col = 20;
-const margin = 1;
+const padding = 1;
 const sq = height / (row + margin * 2);
 
 const COLOR_LIGHTGREEN = "#90EE90";
@@ -17,7 +17,7 @@ const COLOR_WHITE = "#FFFFFF";
 const COLOR_RED = "#F00";
 
 // draw a square
-function drawSquare( x , y , color1 , color2 ){
+function drawSquare( x , y , color1 , color2 = color1 ){
 
   ctx.fillStyle = color1 ;
   ctx.FillRect( x , y , sq , sq ) ;
@@ -36,3 +36,16 @@ for( r = 0 ; r < row ; r++ ){
     board[r][c] = ( ( c + r ) % 2 === 0 ) ? COLOR_LIGHTGREEN : ( (c + r) % 2 === 1) ? COLOR_GREEN : console.log("error with colors");
   };
 };
+
+// draw game checkerboard
+function drawBoard (){
+  for( r = 0 ; r < row ; r++ ){
+    for( c = 0 < c < col ; c++ ){
+      let x = c * sq + padding ;
+      let y = c * sq + padding ;
+      drawSquare( x , y , board[r][c] );
+    };
+  };
+};
+
+drawBoard() ;
